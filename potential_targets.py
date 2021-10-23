319b07583fedbd9fb352622feb735b960b2fc036
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-10-24 00:41:55 +0100

    open and read .csv file
    This file has a list of potential targets along the Galactic plane

--- potential_targets.py
+++ potential_targets.py
@@ -1,1 +1,6 @@
+# Open the .csv file, which contains data of potential targets from Table 4.1 (mod. website). This prints out the table in basic format, showing the RA/Dec of 
+# potential targets (as well as the Galactic longitudes)
 
+with open('gal_coords_targets.csv', mode='r') as galactic_coords:
+    read_file = galactic_coords.read()
+    print(read_file)
