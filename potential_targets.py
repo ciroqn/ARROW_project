17d3ddd0d7f9b71d3ff154a6357ff690189e0813
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-10-24 09:31:02 +0100

    create data frame from pandas
    DF for Ra/Dec values in csv file

--- potential_targets.py
+++ potential_targets.py
@@ -5,5 +5,14 @@ with open('gal_coords_targets.csv', mode='r') as galactic_coords:
     read_file = galactic_coords.read()
     print(read_file)
     
-# Import pandas, since we will need this for the 'read_csv' method
+# Import pandas, since we will need this for the 'read_csv' method, and this printout is much clearer than above
+
 import pandas as pd
+
+# Creating a Pandas DataFrame (a more powerful array), using comma-separated variable file, and reading file *from* header (or, row) 1:
+
+df = pd.read_csv('gal_coords_targets.csv', sep=',', header=1)
+
+
+                 
+
