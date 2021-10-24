50ce94f31828868f3e16014ccf30e351fbf0c0a6
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-10-24 10:14:16 +0100

    import packages pandas and satrapy

--- select_coords_tracking.py
+++ select_coords_tracking.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+from astropy import constants as c
+from astropy import units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.time import Time
