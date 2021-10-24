bdbc4908d849290f2e41c9e2dbe84e8fe4b55aea
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-10-24 17:29:09 +0100

    no need for certain imports

--- select_coords_tracking.py
+++ select_coords_tracking.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-from astropy import constants as c
 from astropy import units as u
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
