f18635216883b2d62d6d7b3943e0741641188192
MW <79422987+ciroqn@users.noreply.github.com>
2021-10-27 19:25:43 +0100

    set up skycoords for park and nep positions

--- slew_times.py
+++ slew_times.py
@@ -2,3 +2,12 @@ from astropy import constants as const
 from astropy import units as u
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
+
+#<------------------------------- SLEW FROM PARK TO NEP -------------------------------->
+
+# First create SkyCoords for Parking Position of ARROW and NEP. Az/Alt of Park is (180, 80) deg.
+park_radec = SkyCoord('22h21m00s',  '+42d04m00s')  # Approx. RA/Dec coords of Park position on 4/11/21 at ~1930 
+nep_radec = SkyCoord('18h00m00s', '+66d33m00s')
+
+# Use .separation() method to measure angular distance between them
+ang_distance = park_radec.separation(nep_radec)
