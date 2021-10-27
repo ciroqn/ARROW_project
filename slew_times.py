bfa50134652e5889c7081bfa861624bd60dea05d
MW <79422987+ciroqn@users.noreply.github.com>
2021-10-27 19:28:14 +0100

    get vars for t=d/v to get slew time

--- slew_times.py
+++ slew_times.py
@@ -11,3 +11,22 @@ nep_radec = SkyCoord('18h00m00s', '+66d33m00s')
 
 # Use .separation() method to measure angular distance between them
 ang_distance = park_radec.separation(nep_radec)
+
+# Save in variable whose units are in degrees, instead of d:m:s
+ang_dist_deg = ang_distance.deg*u.deg
+
+# Check
+print(ang_dist_deg)
+
+print('The angular distance of Park and NEP against the sky is', ang_distance)
+
+# ARROW travels approx. 1 degree every 6 seconds
+travel_speed = 1/6*(u.deg/u.second)
+
+# Check
+print(travel_speed)
+
+slew_time = ang_dist_deg / travel_speed
+
+# Convert to minutes (equal to 4.2 mins)
+print(slew_time.to('min')). 
