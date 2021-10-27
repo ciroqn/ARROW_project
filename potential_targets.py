18ad4ed2c3056ed8ccf7063e65b151976b0a03f6
MW <79422987+ciroqn@users.noreply.github.com>
2021-10-27 18:36:26 +0100

    correct typo in print statement

--- potential_targets.py
+++ potential_targets.py
@@ -121,7 +121,7 @@ for altaz_set in altaz_coords:
     if int(altaz_set.az.degree) > 20 and int(altaz_set.az.degree) < 340 and int(altaz_set.alt.degree) > 20 and int(altaz_set.alt.degree) < 80:
         print(f'The Alt/Az coordinates, with Azimuth {altaz_set.az.deg:.3f} and Altitude {altaz_set.alt.deg:.3f}, are in range, and are VISIBLE at', t_str, '; RA/Dec: (', df['RA'][count], ',', df['DEC'][count],'); (l =', df['l'][count] ,')')
     else:
-        print(f'The Alt/Az coordinates, with Azimuth {altaz_set.az.deg:.3f} and Altitude {altaz_set.alt.deg:.3f}, are in NOT in range at', t_str, '; RA/Dec: (', df['RA'][count], ',', df['DEC'][count],'); (l =', df['l'][count] ,')')
+        print(f'The Alt/Az coordinates, with Azimuth {altaz_set.az.deg:.3f} and Altitude {altaz_set.alt.deg:.3f}, are NOT in range at', t_str, '; RA/Dec: (', df['RA'][count], ',', df['DEC'][count],'); (l =', df['l'][count] ,')')
     count += 1
     
     
